309faa1b1eb15b11ee2133d7a2ba953784751f7c
Bakha022 <abduqahhorvbahodir536@gmail.com>
2025-02-28 16:02:14 +0500

    task 3

--- main.ts
+++ main.ts
@@ -6,11 +6,24 @@
 
 // console.log(Kata.disemvowel('This website is for losers LOL!'))
 
-export function countBits(n: number): number {
-	let sum: number = 0
-	n.toString(2)
-		.split('')
-		.forEach(element => (element == "1" ? (sum += 1) : sum)) 
-	return sum
+// export function countBits(n: number): number {
+// 	let sum: number = 0
+// 	n.toString(2)
+// 		.split('')
+// 		.forEach(element => (element == "1" ? (sum += 1) : sum))
+// 	return sum
+// }
+// console.log(countBits(7))
+
+export class Kata {
+	static squareDigits(num: number): number {
+		let number = ''
+		num
+			.toString()
+			.split('')
+			.forEach(element => (number += +element * (+element)))
+		return +number
+	}
 }
-console.log(countBits(7)) 
+
+console.log(Kata.squareDigits(9119));
