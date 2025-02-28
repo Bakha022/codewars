cbd2656a20eaa2b7e4046eabaf6812d15b15d969
Bakha022 <abduqahhorvbahodir536@gmail.com>
2025-02-28 16:26:39 +0500

    4 task

--- main.ts
+++ main.ts
@@ -15,15 +15,35 @@
 // }
 // console.log(countBits(7))
 
-export class Kata {
-	static squareDigits(num: number): number {
-		let number = ''
-		num
-			.toString()
-			.split('')
-			.forEach(element => (number += +element * (+element)))
-		return +number
-	}
+// export class Kata {
+// 	static squareDigits(num: number): number {
+// 		let number = ''
+// 		num
+// 			.toString()
+// 			.split('')
+// 			.forEach(element => (number += +element * (+element)))
+// 		return +number
+// 	}
+// }
+
+// console.log(Kata.squareDigits(9119));
+
+export function isValidWalk(walk: string[]) {
+	if (walk.length !== 10) return false
+	let north = 0,
+		south = 0,
+		west = 0,
+		east = 0
+	walk.forEach(element => {
+		if (element == 'n') north++
+		else if (element == 's') south++
+		else if (element == 'w') west++
+		else if (element == 'e') east++
+	})
+
+	return north === south && east === west
 }
 
-console.log(Kata.squareDigits(9119));
+console.log(isValidWalk(['n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's'])) // true
+console.log(isValidWalk(['n', 'n', 'n', 's', 's', 's', 'e', 'w', 'e', 'w'])) // false
+console.log(isValidWalk(['n', 's']))
