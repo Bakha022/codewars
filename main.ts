505b39621f8e7beb890feb30633d89694a0977fe
Bakha022 <abduqahhorvbahodir536@gmail.com>
2025-02-28 15:51:40 +0500

    2-task end

--- main.ts
+++ main.ts
@@ -1,7 +1,16 @@
-export class Kata {
-	static disemvowel(str: string) {
-		return str.replace(/[aeiouAEIOU]/g, '')
-	}
-}
+// export class Kata {
+// 	static disemvowel(str: string) {
+// 		return str.replace(/[aeiouAEIOU]/g, '')
+// 	}
+// }
 
-console.log(Kata.disemvowel('This website is for losers LOL!'))
+// console.log(Kata.disemvowel('This website is for losers LOL!'))
+
+export function countBits(n: number): number {
+	let sum: number = 0
+	n.toString(2)
+		.split('')
+		.forEach(element => (element == "1" ? (sum += 1) : sum)) 
+	return sum
+}
+console.log(countBits(7)) 
